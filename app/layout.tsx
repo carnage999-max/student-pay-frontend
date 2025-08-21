fd897f7d519747ddfeaa76f97d3d0a7e161ab8d2
carnage999-max <jamesezekiel039@gmail.com>
2025-08-22 00:55:39 +0100

    feat(metadata): added opengraph image support along with apple-icon and icon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="description"
+          content="Welcome to StudentPay - Seamless payment and receipt generation for university departments and students. Secure. Simple. Fast."
+        />
+        <meta
+          name="keywords"
+          content="payment receipt generation, university payments, student payments, department management"
+        />
+      </head>
       <body>
         {children}
       </body>
