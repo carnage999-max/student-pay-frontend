d8a8c7ac4541f49290bc00428b112f81ab22d431
carnage999-max <jamesezekiel039@gmail.com>
2025-08-22 01:22:48 +0100

    feat(metadata): added metadata to rootlayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,33 @@
 import "./globals.css";
 
+export const metadata = {
+  description: "Welcome to StudentPay - Seamless payment and receipt generation for university departments and students. Secure. Simple. Fast.",
+  generator: 'Next.js',
+  title: 'Student Pay',
+  applicationName: 'Student Pay',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['payment', 'receipt generation', 'university payments', 'departments', 'students', 'secure', 'simple', 'fast', 'StudentPay'],
+  authors: [{ name: 'Ezekiel Okebule' , url: 'https://github.com/carnage999-max'},],
+  creator: 'Ezekiel Okebule(11011)',
+  publisher: '11011',
+  openGraph: {
+    title: 'Student Pay',
+    description: 'Seamless payment and receipt generation for university departments and students. Secure. Simple. Fast.',
+    url: 'https://student-pay.sevalla.app',
+    siteName: 'Student Pay',
+    images: [
+      {
+        url: '/images/og-img.png',
+        width: 1200,
+        height: 630,
+        alt: 'Student Pay - Seamless payment and receipt generation for university departments and students.',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -7,16 +35,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="description"
-          content="Welcome to StudentPay - Seamless payment and receipt generation for university departments and students. Secure. Simple. Fast."
-        />
-        <meta
-          name="keywords"
-          content="payment receipt generation, university payments, student payments, department management"
-        />
-      </head>
       <body>
         {children}
       </body>
